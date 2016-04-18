5b7aec919e7f0ba3dd3654031ab913a355adf75d
Julien Letessier <julien.letessier@gmail.com>
2016-04-18 16:11:18 +0200

    Makes deploy adapter generic

--- app/services/deploy_fetcher.rb
+++ app/services/deploy_fetcher.rb
@@ -2,8 +2,13 @@ require 'deploy_fetcher/generic'
 require 'deploy_fetcher/honeybadger_adapter'
 
 module DeployFetcher
-  def self.build(logger)
-    adapter = HoneybadgerAdapter.new(logger)
+  def self.build(logger, adapter: nil)
+    adapter ||= ENV.fetch('ADAPTER_FETCH', 'HONEYBADGER')
+    klass = case adapter.downcase
+            when 'honeybadger' then HoneybadgerAdapter
+            else raise ArgumentError, "unknown adapter '#{adapter}'"
+            end
+    adapter = klass.new(logger)
     Generic.new(adapter, logger)
   end
 end
